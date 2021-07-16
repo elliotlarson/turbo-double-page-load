c4f074a0eae4e34a8464be8daa9948e488be0d88
elliotlarson <me@elliot.la>
2021-07-15 19:16:14 -0700

    Init commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def page1
+  end
+
+  def page2
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'pages/page1'
+  get 'pages/page2'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
